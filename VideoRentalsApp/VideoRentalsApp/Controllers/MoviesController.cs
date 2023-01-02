06e0fd095c740aa3577df7caac0f10a5e70583c5
Nick <pawsey18@gmail.com>
2023-01-02 18:08:30 -0400

    Initial Commit

---
+++ VideoRentalsApp/VideoRentalsApp/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRentalsApp.Models;
+using VideoRentalsApp.ViewModels;
+
+namespace VideoRentalsApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "I know what you did last summer"
+            };
+
+            var customers = new List<Customer>
+            {
+
+             new Customer
+             {
+                 Name = "Customer 1"
+             },
+             new Customer
+             {
+                 Name = " Customer 2"
+             }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
